7543d9e0b3ebe50472c4fd1ef0caebd79935653c
Joe <joe@yis-macbook-pro.lan>
2020-12-05 16:44:09 -0800

    Use of json for s3 buckets

--- udacity/etl.py
+++ udacity/etl.py
@@ -12,11 +12,20 @@ import json
 import configparser
 from pyspark.sql import SQLContext
 import datetime
+
+file = open('./s3.json', "r")
+s3_config = json.loads(file.read())
+file.close()
+
 def frame_row_from_json(json, spark):
     """frame_row_from_json
     This needs to exist as pyspark can't load in memory json directly
     it only supports reading files from disk. So here we write the json
     payload to a temp file on disc then read it back with spark.
+    Parameters:
+        json {string}: Json buffer to turn into a data frame
+        spark {Spark}: The spark client handle
+    Return: PandasDataFrame: The data frame for the row created by the in memory json
     """
     file = "./temp"
     f = open(file, "w")
@@ -27,6 +36,9 @@ def frame_row_from_json(json, spark):
 def process_songs(spark, bucket):
     """process_songs
     Process all song files from the source s3 bucket by inserting artist and song into s3
+    Parameters:
+        spark {Spark}: The spark client handle
+        bucket {Bucket}: The S3 bucket containing the data
     """
     
     artists = frame_row_from_json(json.dumps({
@@ -63,29 +75,43 @@ def process_songs(spark, bucket):
                 "duration": songRec["duration"]
             }), spark))
 
-    spark.createDataFrame(artists).write.parquet("s3a://scpro2-udacity-data-engineering-output/artists.parquet", mode="overwrite")
-    spark.createDataFrame(songs).write.parquet("s3a://scpro2-udacity-data-engineering-output/songs.parquet", mode="overwrite")
+    spark.createDataFrame(artists).write.parquet(s3_config["artistsParquet"], mode="overwrite")
+    spark.createDataFrame(songs).write.parquet(s3_config["songsParquet"], mode="overwrite")
 
 def artist_id_from_name(name, spark):
     """artist_id_from_name
     Get the artist ID from the artist name
+    Parameters:
+        name {String}: Name of the artist whose ID we want
+        spark {Spark}: The spark client handle
     """
-    return SQLContext(spark.sparkContext).read.parquet("s3a://scpro2-udacity-data-engineering-output/artists.parquet"
+    if name is None:
+        return None
+    
+    list = SQLContext(spark.sparkContext).read.parquet(s3_config["artistsParquet"]
         ).select("artist_id"
-        ).where("name = '{}'".format(name)).take(1)[0].artist_id
+        ).where("name = '{}'".format(name.replace("'", "''"))).take(1)
+    return None if len(list) == 0 else list[0].artist_id
     
 
 def song_id_from_title(title, spark):
     """song_id_from_title
     Get the song ID from the song title
     """
-    return SQLContext(spark.sparkContext).read.parquet("s3a://scpro2-udacity-data-engineering-output/songs.parquet"
-        ).select("song_id"
-        ).where("title = '{}'".format(title)).take(1)[0].song_id
+    if title is None:
+        return None
     
-def process_logs(artistsAndSongs, spark, bucket):
+    list = SQLContext(spark.sparkContext).read.parquet(s3_config["songsParquet"]
+        ).select("song_id"
+        ).where("title = '{}'".format(title.replace("'", "''"))).take(1)
+    return None if len(list) == 0 else list[0].song_id
+    
+def process_logs(spark, bucket):
     """process_logs
     Process log files from source s3 by inserting the song play and user into s3
+    Parameters:
+        spark {Spark}: The spark client handle
+        bucket {Bucket}: The S3 bucket containing the data
     """
     
     dayOfTheWeek = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
@@ -156,16 +182,21 @@ def process_logs(artistsAndSongs, spark, bucket):
                     'user_agent': log['userAgent']
                 }), spark))
                 
-    spark.createDataFrame(time).write.parquet("s3a://scpro2-udacity-data-engineering-output/time.parquet", mode="overwrite")
-    spark.createDataFrame(users).write.parquet("s3a://scpro2-udacity-data-engineering-output/users.parquet", mode="overwrite")
-    spark.createDataFrame(plays).write.parquet("s3a://scpro2-udacity-data-engineering-output/songplays.parquet", mode="overwrite")
+    spark.createDataFrame(time).write.parquet(s3_config["timeParquet"], mode="overwrite")
+    spark.createDataFrame(users).write.parquet(s3_config["usersParquet"], mode="overwrite")
+    spark.createDataFrame(plays).write.parquet(s3_config["songplaysParquet"], mode="overwrite")
     
 def print_parquet_files(ctx):
-    ctx.read.parquet("s3a://scpro2-udacity-data-engineering-output/artists.parquet").show()
-    ctx.read.parquet("s3a://scpro2-udacity-data-engineering-output/songs.parquet").show()
-    ctx.read.parquet("s3a://scpro2-udacity-data-engineering-output/time.parquet").show()
-    ctx.read.parquet("s3a://scpro2-udacity-data-engineering-output/users.parquet").show()
-    ctx.read.parquet("s3a://scpro2-udacity-data-engineering-output/songplays.parquet").show()
+    """print_parquet_files
+    Print the values of the parquet files in the output bucket
+    Parameters:
+        ctx {SparkContext}: The spark cluster context
+    """
+    ctx.read.parquet(s3_config["artistsParquet"]).show()
+    ctx.read.parquet(s3_config["songsParquet"]).show()
+    ctx.read.parquet(s3_config["timeParquet"]).show()
+    ctx.read.parquet(s3_config["usersParquet"]).show()
+    ctx.read.parquet(s3_config["songplaysParquet"]).show()
     
 def main():
     spark = SparkSession.builder.config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0").getOrCreate()
@@ -177,7 +208,7 @@ def main():
     spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", creds['keySec'])
 
     s3 = boto3.resource('s3', aws_access_key_id=creds['keyId'], aws_secret_access_key=creds['keySec'])
-    bucket = s3.Bucket('scpro2-udacity-data-engineering')
+    bucket = s3.Bucket(s3_config["sourceBucket"])
     
     process_songs(spark, bucket)
     process_logs(spark, bucket)
